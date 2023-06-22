2559b489281341ce7482d031863aec96ac447978
ReyZ <125138380+ReyZ0309@users.noreply.github.com>
2023-06-22 19:17:07 +0200

    Add files via upload

---
+++ Bonus/config.py
@@ -0,0 +1,49 @@
+ ##################################################################
+#                                                                  #
+#  $$$$$$\                       $$$$$$\  $$\                      #
+# $$  __$$\                     $$  __$$\ \__|                     #
+# $$ /  \__| $$$$$$\  $$$$$$$\  $$ /  \__|$$\  $$$$$$\   $$$$$$$\  #
+# $$ |      $$  __$$\ $$  __$$\ $$$$\     $$ |$$  __$$\ $$  _____| #
+# $$ |      $$ /  $$ |$$ |  $$ |$$  _|    $$ |$$ /  $$ |\$$$$$$\   #
+# $$ |  $$\ $$ |  $$ |$$ |  $$ |$$ |      $$ |$$ |  $$ | \____$$\  #
+# \$$$$$$  |\$$$$$$  |$$ |  $$ |$$ |      $$ |\$$$$$$$ |$$$$$$$  | #
+#  \______/  \______/ \__|  \__|\__|      \__| \____$$ |\_______/  #
+#                                             $$\   $$ |           #
+#                                             \$$$$$$  |           #
+#                                              \______/            #
+ ##################################################################
+
+
+#The bot prefix
+Prefix="!"
+#
+channelname = "your channel name"
+#
+SpamMSG = "your spam message"
+#
+UserID = your user ID
+#if set to True it will ban everyone in the server if you use the Nuke command
+auto_ban = True
+
+#
+rolename = "rolename"
+
+#[0=OFF] [1=ADMIN PERMS] [2=NONE PERMS]
+auto_everyperm = 0
+
+#Does random charracters instead rolename and channelname
+#so that no antiraid bot can remove them
+Antiraidbotbypass = True
+
+
+#Basicly the embed color
+Embedcolor = 0xff085f
+
+
+#Paste here all bottokens for the DMSpamer!
+
+
+#
+
+NukeToken = "token"
+
